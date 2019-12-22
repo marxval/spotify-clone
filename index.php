0ab29b502bf6a213027b2b5c847885d3205d9323
Martín Valdivia Franco <mxmghost@gmail.com>
2019-12-22 14:48:54 -0600

    Progress bar added

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include("includes/config.php");
-session_destroy();
+// session_destroy();
 
 if (isset($_SESSION['userLoggedIn'])) {
     $userLoggedIn = $_SESSION['userLoggedIn'];
@@ -18,10 +18,54 @@ if (isset($_SESSION['userLoggedIn'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Splotify</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 
 <body>
-    Hola amigo felicidades
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+
+            </div>
+            <div id="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="controlButton shuffle" title="Shuffle button">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
+                        <button class="controlButton previous" title="Previous button">
+                            <img src="assets/images/icons/previous.png" alt="Previous">
+                        </button>
+                        <button class="controlButton play" title="Play button">
+                            <img src="assets/images/icons/play.png" alt="Play">
+                        </button>
+                        <button class="controlButton pause" title="Pause button" style="display: none">
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </button>
+                        <button class="controlButton next" title="Next button">
+                            <img src="assets/images/icons/next.png" alt="Next">
+                        </button>
+                        <button class="controlButton repeat" title="Repeat button">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </button>
+                    </div>
+                    <div class="playbackBar">
+                        <span class="progressTime current">0.00</span>
+                        <div class="progressBar">
+                            <div class="progressBarBg">
+                                <div class="progress"></div>
+                            </div>
+                        </div>
+                        <span class="progressTime remaining">0.00</span>
+
+                    </div>
+                </div>
+            </div>
+            <div id="nowPlayingRight">
+
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
